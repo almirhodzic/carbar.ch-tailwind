05d8e014212c9ccb69a48897102af7913c32346f
almirh <24458972+almirhodzic@users.noreply.github.com>
2023-01-17 16:09:47 +0100

    New optimizings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: [
+    './dist/**/*.{html,js}',
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
 
   darkMode: 'class', // or 'media' or 'class'
 
   theme: {
+    screens: {
+      'xss': '320px',
+      'xs': '360px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     debugScreens: {
       position: ['bottom', 'left'],
     },
@@ -19,5 +31,6 @@ module.exports = {
   },
   plugins: [
     require('tailwindcss-debug-screens'),
+    require('tw-elements/dist/plugin')
   ],
 }
