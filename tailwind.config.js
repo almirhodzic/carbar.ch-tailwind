be8c280b65abf6fceac92169fe6b42a40f4bc147
almirh <24458972+almirhodzic@users.noreply.github.com>
2023-01-17 23:19:15 +0100

    Global Changes Styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,24 +9,29 @@ module.exports = {
 
   theme: {
     screens: {
-      'xss': '320px',
-      'xs': '360px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+    'xss': '320px',
+    'xs': '360px',
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    'xl': '1280px',
+    '2xl': '1536px',
     },
     debugScreens: {
       position: ['bottom', 'left'],
     },
     extend: {
-      colors : {
-        maincolor : '#ff0080',
-        maincolorhover : '#d8066f',
-        backgroundcolor : '#fad3d9',
-        backgrounddark : '#05080e'
-        },
+      fontFamily: {
+        'swanky': ['Swanky and Moo Moo', 'cursive'],
+        'turncoat': ['Walter Turncoat', 'cursive'],
+        'praise': ['Praise', 'cursive'],
+      },
+    colors : {
+      'maincolor' : '#ff0080',
+      'maincolorhover' : '#d8066f',
+      'backgroundcolor' : '#fad3d9',
+      'backgrounddark' : '#05080e'
+      },
     },
   },
   plugins: [
